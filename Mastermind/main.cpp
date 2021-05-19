b56e7bb6ad6c93598f5d65c7377f0b43308b6a9d
MarVin1006 <marco.tallarino@gmail.com>
2021-05-19 20:16:48 +0200

    Added Mastermind

---
+++ Mastermind/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+int randn(int num)
+{
+    return (rand() % 10);
+}
+
+char vuoicontinuare()
+{
+    char x
+    do
+    cout << "Vuoi continuare(S o N)? "
+    cin >>
+    while( x=!'s' || x=!'S' || x=!'n' || x=!'N')
+}
+
+int main()
+{
+    srand(time(NULL));
+    int tentativi = 0, matrice[4][4];
+
+
+
+    return 0;
+}
