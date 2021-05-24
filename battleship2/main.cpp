adf83657f4343f169ee694fe5cf6ca8e8b4d3a52
MarVin1006 <marco.tallarino@gmail.com>
2021-05-24 11:37:53 +0200

    Created battleship2

---
+++ battleship2/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+const int LEN=10;
+
+void print(char v[][])
+
+int main()
+{
+    char battlefield[LEN][LEN];
+    cout<<"Programma BattleShip2 in esecuzione"<<endl;
+
+
+    return 0;
+}
