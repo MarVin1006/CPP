f659ccba3981bc926a07ec101ac6a3c202f5e845
MarVin1006 <marco.tallarino@gmail.com>
2021-05-24 11:51:31 +0200

    Made print function in Battleship2

--- battleship2/main.cpp
+++ battleship2/main.cpp
@@ -3,13 +3,22 @@ using namespace std;
 
 const int LEN=10;
 
-void print(char v[][])
+void print(char m[LEN][LEN])
+{
+    for(int i=0; i<LEN; i++){
+        for(int j=0; j<LEN; j++){
+            m[i][j]={'o'};
+            cout<< m[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
 
 int main()
 {
-    char battlefield[LEN][LEN];
+    char bf[LEN][LEN];
     cout<<"Programma BattleShip2 in esecuzione"<<endl;
-
+    print(bf);
 
     return 0;
 }
