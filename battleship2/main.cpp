b53c4f49cc9cf93b57aee1b0145f4430a0c82553
MarVin1006 <marco.tallarino@gmail.com>
2021-05-26 13:22:46 +0200

    Added horizontal ships in Battleship2

--- battleship2/main.cpp
+++ battleship2/main.cpp
@@ -1,24 +1,49 @@
 #include <iostream>
+#include <time.h>
 using namespace std;
 
 const int LEN=10;
+char bf[LEN][LEN];
 
-void print(char m[LEN][LEN])
+void clear(){
+    for (int i=0; i<LEN; i++){
+        for (int j=0; j<LEN; j++){
+            bf[i][j]=' ';
+        }
+    }
+}
+
+void print()
 {
+    for(int i=0; i<LEN; i++){cout << "--";} cout << endl;
     for(int i=0; i<LEN; i++){
         for(int j=0; j<LEN; j++){
-            m[i][j]={'o'};
-            cout<< m[i][j] << " ";
+            cout<< bf[i][j] << ' ';
         }
         cout << endl;
     }
+    for(int i=0; i<LEN; i++){cout << "--";} cout << endl;
+}
+
+void ship(int l){
+    int row=rand()%LEN;
+    int column=rand()%(LEN-l);
+    cout << "riga= "<<row<<" colonna= "<<column<<endl;
+    for (int i=column; i<column+l; i++){
+        bf[row][i]='X';
+    }
+
 }
 
 int main()
 {
-    char bf[LEN][LEN];
+    srand(time(NULL));
+    clear();   
     cout<<"Programma BattleShip2 in esecuzione"<<endl;
-    print(bf);
+    ship(3);
+    ship(4);
+    ship(7);
+    print();
 
     return 0;
 }
